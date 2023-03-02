c58e61d05182b6c989fce87dbaa91d2c84e84395
sazurov <sazurov>
2023-03-02 13:50:43 +0300

    cycle

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine($"Введите a");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Введите b");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Введите c");
+
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите последний промежуток");
+
+int d = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите х");
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+for (d++; d < 2; x++,d++)
+{
+    Console.WriteLine($"{a* Math.Pow(x , x) * b * x + c}");
+    Console.Write($"{x}:");
+    
+}
+
